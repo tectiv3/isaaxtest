94ed9cf11236a337557b62a489b4425e44adc074
tectiv3 <git@getpanda.me>
2018-02-03 15:12:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,5 @@
 import time
-import Adafruit_DHT
 
-sensor = Adafruit_DHT.DHT22
-pin = 4
-
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
 while True:
-    
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
-    time.sleep(120.0)
+	print("Isaax test")
+	time.sleep(15)
