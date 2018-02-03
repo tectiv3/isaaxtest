f4ab1f8c11c2ad0fd9d2c81b8e043d6b11b6b72b
tectiv3 <git@getpanda.me>
2018-02-03 20:56:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 # guarantee the timing of calls to read the sensor).
 # If this happens try again!
 while True:
-    
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
     if humidity is not None and temperature is not None:
         print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
     else:
